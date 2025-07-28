aed503ff0e78451dba141c4477824cc1f47c795a
Zeslley <wesllet3@gmail.com>
2025-07-28 01:43:40 -0300

    Ajustando as animações e lógica

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const optionImages = document.querySelectorAll('.option-image');
+const container = document.querySelector('.container');
+const resultText = document.querySelector('.result-text');
+const computerImg =['img/pedra.png', 'img/papel.png', 'img/tesoura.png']; 
+
 
 function handleOptionClick(event) {
     const  clickedImage = (event.currentTarget)
+
+    container.classList.add('start');
+    resultText.textContent = 'Pensando...';
+
+    setTimeout(() => {
+        container.classList.remove('start')
+
+        const randomNumber = Math.floor(Math.random() * computerImg.length);
+
+
+        resultText.textContent = "X ganhou!"
+    }, 2000);
 }
 
 optionImages.forEach( img => {
